31759086a1c8b17bf73eba0b012fd391bd69b596
Sarah dePalo <sarahdepalo99@gmail.com>
2021-06-02 15:20:05 -0400

    added basic modal functionality

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,6 @@ function toggleModal () {
     overlay.classList.toggle('visible');
 }
 
-toggleModal();
 
 const closeModalBtn = document.querySelector('#closeModal');
 
@@ -13,3 +12,35 @@ closeModalBtn.addEventListener('click', function () {
     toggleModal();
 })
 
+// const overlay = document.querySelector('#overlay');
+// overlay.addEventListener('click', function () {
+//     toggleModal();
+// })
+
+function buildQuote(theQuote) {
+    const modalElement = document.querySelector('#modal p');
+    modalElement.innerText = theQuote;
+    toggleModal();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetch(`https://api.chucknorris.io/jokes/random?category=dev`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            buildQuote(data.value);
+        })
+        .catch(function(error){
+            console.error("ERROR: ", error);
+            return error;
+        });
+
+        // Allows for any key you press to open and close the pop-up:
+        // document.addEventListener('keydown', function(event) {
+        //     console.log("The key that was pressed: ", event.key)
+        //     if (event.key === 'Escape') {
+        //         toggleModal();
+        //     }
+        // })
+})
